51fb33d9f0b5a620153e43b6b81d673266d5a477
Shingo Ishimura <metal.tie@gmail.com>
2015-07-07 14:33:17 +0900

    適当に指定したKindをDatastore Admin Backで、Cloud Storageに出力するサンプルを作ってみた

--- src/datastore_backup_shortcut.go
+++ src/datastore_backup_shortcut.go
@@ -1,6 +1,10 @@
 package datastore_backup_shortcut
 
 import (
+	"appengine"
+	"appengine/datastore"
+	"appengine/taskqueue"
+	"fmt"
 	"net/http"
 )
 
@@ -9,5 +13,26 @@ func init() {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	// some code
+	ac := appengine.NewContext(r)
+	ac.Infof(r.URL.String())
+
+	f(ac, r.URL.String()[61:len(r.URL.String())])
+	fmt.Fprint(w, "Done")
+}
+
+func f(c appengine.Context, url string) {
+	path := fmt.Sprintf("/_ah/datastore_admin/backup.create?name=BackupToCloud&filesystem=gs&%s", url)
+	c.Infof("Path : %s", path)
+	err := datastore.RunInTransaction(c, func(c appengine.Context) error {
+		t := taskqueue.Task{
+			Path:   path,
+			Method: "GET",
+		}
+		_, err := taskqueue.Add(c, &t, "datastore-backup-shortcut")
+		return err
+	}, nil)
+
+	if err != nil {
+		c.Errorf(err.Error())
+	}
 }
